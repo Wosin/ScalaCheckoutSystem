7a791042664ed65e37d3ffaf7d485b1d55fe8323
DominWos <wossyn@gmail.com>
2017-10-16 21:53:35 +0200

    Second part started

--- src/main/scala/Fruits.scala
+++ src/main/scala/Fruits.scala
@@ -1,13 +1,11 @@
-
-
+import Promotions._
 trait Fruit
 case object Orange extends Fruit
 case object Apple extends Fruit
 
 object Fruits {
-
   val fruitPrices = Map[Fruit, Double](Orange -> 0.25, Apple -> 0.60)
-
+  val fruitPromotions = Map[Fruit, (Int => Int)](Apple -> twoForOnePromotion, Orange -> threeForTwoPromotion)
   private def fruitFromString(fruitString: String): Fruit = {
     fruitString.toLowerCase match {
       case "orange" | "o" => Orange
@@ -20,6 +18,8 @@ object Fruits {
   }
 
   def fruitsToPrices(entry: (Fruit, Int)): Double = {
+    val promoFunction = fruitPromotions.getOrElse(entry._1, _)
+    val numberOfItemsAfterPromotion = promoFunction(entry)
     fruitPrices.getOrElse(entry._1, 0.0) * entry._2
   }
 
---
+++ src/main/scala/Promotions.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Wosin on 16.10.2017.
+  */
+object Promotions {
+
+  def twoForOnePromotion(beforePromo: Int): Int = beforePromo/2
+  def threeForTwoPromotion(beforePromo: Int): Int = (2 * beforePromo)/3
+}
